44643bdf0ae9054cfa2adace3ac5e87fa9e021e6
vladimir <gaidukov76@gmail.com>
2020-08-21 20:59:57 +0500

    reformat

--- rtspserver/src/main/java/com/pedro/rtspserver/RtspServer.kt
+++ rtspserver/src/main/java/com/pedro/rtspserver/RtspServer.kt
@@ -20,195 +20,225 @@ import java.util.*
  * TODO Use different session per client.
  */
 
-class RtspServer(context: Context, private val connectCheckerRtsp: ConnectCheckerRtsp,
-  val port: Int) {
+class RtspServer(
+    context: Context, private val connectCheckerRtsp: ConnectCheckerRtsp,
+    val port: Int
+) {
 
-  private val TAG = "RtspServer"
-  private var server: ServerSocket? = null
-  val serverIp = getIPAddress(true)
-  var sps: ByteBuffer? = null
-  var pps: ByteBuffer? = null
-  var vps: ByteBuffer? = null
-  var sampleRate = 32000
-  var isStereo = true
-  private val clients = mutableListOf<Client>()
-  private var thread: Thread? = null
-  var user: String?= null
-  var password: String?= null
+    private val TAG = "RtspServer"
+    private var server: ServerSocket? = null
+    val serverIp = getIPAddress(true)
+    var sps: ByteBuffer? = null
+    var pps: ByteBuffer? = null
+    var vps: ByteBuffer? = null
+    var sampleRate = 32000
+    var isStereo = true
+    private val clients = mutableListOf<Client>()
+    private var thread: Thread? = null
+    var user: String? = null
+    var password: String? = null
 
-  fun startServer() {
-    stopServer()
-    thread = Thread {
-      server = ServerSocket(port, -1, InetAddress.getByName(serverIp))
-      while (!Thread.interrupted()) {
-        Log.i(TAG, "Server started $serverIp:$port")
-        try {
-          val client =
-            Client(server!!.accept(), serverIp, port, connectCheckerRtsp, sps!!, pps!!, vps, sampleRate,
-              isStereo, user, password)
-          client.start()
-          clients.add(client)
-        } catch (e: SocketException) {
-          Log.e(TAG, "Error", e)
-          break
-        } catch (e: IOException) {
-          Log.e(TAG, e.message ?: "")
-          continue
-        }
-      }
-      Log.i(TAG, "Server finished")
-    }
-    thread?.start()
-  }
-
-  fun stopServer() {
-    clients.forEach { it.stopClient() }
-    clients.clear()
-    thread?.interrupt()
-    try {
-      thread?.join(100)
-    } catch (e: InterruptedException) {
-      thread?.interrupt()
-    }
-    thread = null
-    if (server != null) {
-      if (!server!!.isClosed) server!!.close()
-    }
-  }
-
-  fun setAuthorization(user: String, password: String) {
-    this.user= user;
-    this.password= password
-  }
-
-  fun sendVideo(h264Buffer: ByteBuffer, info: MediaCodec.BufferInfo) {
-    clients.forEach {
-      if (it.isAlive && it.canSend) {
-        it.rtspSender.sendVideoFrame(h264Buffer.duplicate(), info)
-      }
-    }
-  }
-
-  fun sendAudio(aacBuffer: ByteBuffer, info: MediaCodec.BufferInfo) {
-    clients.forEach {
-      if (it.isAlive && it.canSend) {
-        it.rtspSender.sendAudioFrame(aacBuffer.duplicate(), info)
-      }
-    }
-  }
-
-  fun setVideoInfo(sps: ByteBuffer, pps: ByteBuffer, vps: ByteBuffer?) {
-    this.sps = sps
-    this.pps = pps
-    this.vps = vps  //H264 has no vps so if not null assume H265
-  }
-
-  private fun getIPAddress(useIPv4: Boolean): String {
-    try {
-      val interfaces: List<NetworkInterface> =
-        Collections.list(NetworkInterface.getNetworkInterfaces())
-      for (intf in interfaces) {
-        val addrs: List<InetAddress> =
-          Collections.list(intf.inetAddresses)
-        for (addr in addrs) {
-          if (!addr.isLoopbackAddress) {
-            val sAddr = addr.hostAddress
-            val isIPv4 = sAddr.indexOf(':') < 0
-            if (useIPv4) {
-              if (isIPv4) return sAddr
-            } else {
-              if (!isIPv4) {
-                val delim = sAddr.indexOf('%') // drop ip6 zone suffix
-                return if (delim < 0) sAddr.toUpperCase() else sAddr.substring(0, delim)
-                  .toUpperCase()
-              }
+    fun startServer() {
+        stopServer()
+        thread = Thread {
+            server = ServerSocket(port, -1, InetAddress.getByName(serverIp))
+            while (!Thread.interrupted()) {
+                Log.i(TAG, "Server started $serverIp:$port")
+                try {
+                    val client =
+                        Client(
+                            server!!.accept(),
+                            serverIp,
+                            port,
+                            connectCheckerRtsp,
+                            sps!!,
+                            pps!!,
+                            vps,
+                            sampleRate,
+                            isStereo,
+                            user,
+                            password
+                        )
+                    client.start()
+                    clients.add(client)
+                } catch (e: SocketException) {
+                    Log.e(TAG, "Error", e)
+                    break
+                } catch (e: IOException) {
+                    Log.e(TAG, e.message ?: "")
+                    continue
+                }
             }
-          }
+            Log.i(TAG, "Server finished")
         }
-      }
-    } catch (ignored: java.lang.Exception) {
-    } // for now eat exceptions
-    return "0.0.0.0"
-  }
-
-  internal class Client(private val socket: Socket, serverIp: String, serverPort: Int,
-    connectCheckerRtsp: ConnectCheckerRtsp, sps: ByteBuffer,
-    pps: ByteBuffer, vps: ByteBuffer?, private val sampleRate: Int,
-    isStereo: Boolean, user: String?, password: String?) : Thread() {
-
-    private val TAG = "Client"
-    private var cSeq = 0
-    private val output = BufferedWriter(OutputStreamWriter(socket.getOutputStream()))
-    private val input = BufferedReader(InputStreamReader(socket.getInputStream()))
-    val rtspSender = RtspSender(connectCheckerRtsp)
-    private val commandsManager =
-      ServerCommandManager(
-        serverIp,
-        serverPort,
-        socket.inetAddress.hostAddress
-      )
-    var canSend = false
-
-    init {
-      commandsManager.setIsStereo(isStereo)
-      commandsManager.sampleRate = sampleRate
-      commandsManager.setVideoInfo(sps, pps, vps)
-      commandsManager.setAuth(user, password)
+        thread?.start()
     }
 
-    override fun run() {
-      super.run()
-      Log.i(TAG, "New client ${commandsManager.clientIp}")
-      while (!interrupted()) {
+    fun stopServer() {
+        clients.forEach { it.stopClient() }
+        clients.clear()
+        thread?.interrupt()
         try {
-          val request = commandsManager.getRequest(input)
-          Log.i(TAG, "REQUEST---------- $request -------------")
-          cSeq = commandsManager.getCSeq(request) //update cSeq
-          if (cSeq == -1) { //If cSeq parsed fail send error to client
-            output.write(commandsManager.createError(500, cSeq))
-            output.flush()
-            continue
-          }
-          val action = request.split("\n")[0]
-          val response = commandsManager.createResponse(action, request, cSeq)
-          Log.i(TAG, "RESPONSE---------- $response -------------")
-          output.write(response)
-          output.flush()
-
-          if (action.contains("play", true)) {
-            Log.i(TAG, "Protocol ${commandsManager.protocol}")
-            rtspSender.setSocketsInfo(commandsManager.protocol, commandsManager.videoClientPorts,
-              commandsManager.audioClientPorts)
-            rtspSender.setVideoInfo(commandsManager.sps, commandsManager.pps, commandsManager.vps)
-            rtspSender.setAudioInfo(sampleRate)
-            rtspSender.setDataStream(socket.getOutputStream(), commandsManager.clientIp)
-            if (commandsManager.protocol == Protocol.UDP) {
-              rtspSender.setVideoPorts(commandsManager.videoPorts[0], commandsManager.videoPorts[1])
-              rtspSender.setAudioPorts(commandsManager.audioPorts[0], commandsManager.audioPorts[1])
-            }
-            rtspSender.start()
-            canSend = true
-          }
-        } catch (e: SocketException) { // Client has left
-          Log.e(TAG, "Client disconnected", e)
-          break
-        } catch (e: Exception) {
-          Log.e(TAG, "Unexpected error", e)
+            thread?.join(100)
+        } catch (e: InterruptedException) {
+            thread?.interrupt()
         }
-      }
+        thread = null
+        if (server != null) {
+            if (!server!!.isClosed) server!!.close()
+        }
     }
 
-    fun stopClient() {
-      canSend = false
-      rtspSender.stop()
-      interrupt()
-      try {
-        join(100)
-      } catch (e: InterruptedException) {
-        interrupt()
-      } finally {
-        socket.close()
-      }
+    fun setAuthorization(user: String, password: String) {
+        this.user = user;
+        this.password = password
     }
-  }
+
+    fun sendVideo(h264Buffer: ByteBuffer, info: MediaCodec.BufferInfo) {
+        clients.forEach {
+            if (it.isAlive && it.canSend) {
+                it.rtspSender.sendVideoFrame(h264Buffer.duplicate(), info)
+            }
+        }
+    }
+
+    fun sendAudio(aacBuffer: ByteBuffer, info: MediaCodec.BufferInfo) {
+        clients.forEach {
+            if (it.isAlive && it.canSend) {
+                it.rtspSender.sendAudioFrame(aacBuffer.duplicate(), info)
+            }
+        }
+    }
+
+    fun setVideoInfo(sps: ByteBuffer, pps: ByteBuffer, vps: ByteBuffer?) {
+        this.sps = sps
+        this.pps = pps
+        this.vps = vps  //H264 has no vps so if not null assume H265
+    }
+
+    private fun getIPAddress(useIPv4: Boolean): String {
+        try {
+            val interfaces: List<NetworkInterface> =
+                Collections.list(NetworkInterface.getNetworkInterfaces())
+            for (intf in interfaces) {
+                val addrs: List<InetAddress> =
+                    Collections.list(intf.inetAddresses)
+                for (addr in addrs) {
+                    if (!addr.isLoopbackAddress) {
+                        val sAddr = addr.hostAddress
+                        val isIPv4 = sAddr.indexOf(':') < 0
+                        if (useIPv4) {
+                            if (isIPv4) return sAddr
+                        } else {
+                            if (!isIPv4) {
+                                val delim = sAddr.indexOf('%') // drop ip6 zone suffix
+                                return if (delim < 0) sAddr.toUpperCase() else sAddr.substring(
+                                    0,
+                                    delim
+                                )
+                                    .toUpperCase()
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (ignored: java.lang.Exception) {
+        } // for now eat exceptions
+        return "0.0.0.0"
+    }
+
+    internal class Client(
+        private val socket: Socket, serverIp: String, serverPort: Int,
+        connectCheckerRtsp: ConnectCheckerRtsp, sps: ByteBuffer,
+        pps: ByteBuffer, vps: ByteBuffer?, private val sampleRate: Int,
+        isStereo: Boolean, user: String?, password: String?
+    ) : Thread() {
+
+        private val TAG = "Client"
+        private var cSeq = 0
+        private val output = BufferedWriter(OutputStreamWriter(socket.getOutputStream()))
+        private val input = BufferedReader(InputStreamReader(socket.getInputStream()))
+        val rtspSender = RtspSender(connectCheckerRtsp)
+        private val commandsManager =
+            ServerCommandManager(
+                serverIp,
+                serverPort,
+                socket.inetAddress.hostAddress
+            )
+        var canSend = false
+
+        init {
+            commandsManager.setIsStereo(isStereo)
+            commandsManager.sampleRate = sampleRate
+            commandsManager.setVideoInfo(sps, pps, vps)
+            commandsManager.setAuth(user, password)
+        }
+
+        override fun run() {
+            super.run()
+            Log.i(TAG, "New client ${commandsManager.clientIp}")
+            while (!interrupted()) {
+                try {
+                    val request = commandsManager.getRequest(input)
+                    Log.i(TAG, "REQUEST---------- $request -------------")
+                    cSeq = commandsManager.getCSeq(request) //update cSeq
+                    if (cSeq == -1) { //If cSeq parsed fail send error to client
+                        output.write(commandsManager.createError(500, cSeq))
+                        output.flush()
+                        continue
+                    }
+                    val action = request.split("\n")[0]
+                    val response = commandsManager.createResponse(action, request, cSeq)
+                    Log.i(TAG, "RESPONSE---------- $response -------------")
+                    output.write(response)
+                    output.flush()
+
+                    if (action.contains("play", true)) {
+                        Log.i(TAG, "Protocol ${commandsManager.protocol}")
+                        rtspSender.setSocketsInfo(
+                            commandsManager.protocol, commandsManager.videoClientPorts,
+                            commandsManager.audioClientPorts
+                        )
+                        rtspSender.setVideoInfo(
+                            commandsManager.sps,
+                            commandsManager.pps,
+                            commandsManager.vps
+                        )
+                        rtspSender.setAudioInfo(sampleRate)
+                        rtspSender.setDataStream(socket.getOutputStream(), commandsManager.clientIp)
+                        if (commandsManager.protocol == Protocol.UDP) {
+                            rtspSender.setVideoPorts(
+                                commandsManager.videoPorts[0],
+                                commandsManager.videoPorts[1]
+                            )
+                            rtspSender.setAudioPorts(
+                                commandsManager.audioPorts[0],
+                                commandsManager.audioPorts[1]
+                            )
+                        }
+                        rtspSender.start()
+                        canSend = true
+                    }
+                } catch (e: SocketException) { // Client has left
+                    Log.e(TAG, "Client disconnected", e)
+                    break
+                } catch (e: Exception) {
+                    Log.e(TAG, "Unexpected error", e)
+                }
+            }
+        }
+
+        fun stopClient() {
+            canSend = false
+            rtspSender.stop()
+            interrupt()
+            try {
+                join(100)
+            } catch (e: InterruptedException) {
+                interrupt()
+            } finally {
+                socket.close()
+            }
+        }
+    }
 }
--- rtspserver/src/main/java/com/pedro/rtspserver/RtspServerCamera2.kt
+++ rtspserver/src/main/java/com/pedro/rtspserver/RtspServerCamera2.kt
@@ -15,99 +15,104 @@ import java.nio.ByteBuffer
 @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
 class RtspServerCamera2 : Camera2Base {
 
-  private val rtspServer: RtspServer
+    private val rtspServer: RtspServer
 
-  constructor(openGlView: OpenGlView, connectCheckerRtsp: ConnectCheckerRtsp, port: Int) : super(
-    openGlView) {
-    rtspServer = RtspServer(openGlView.context, connectCheckerRtsp, port)
-  }
+    constructor(openGlView: OpenGlView, connectCheckerRtsp: ConnectCheckerRtsp, port: Int) : super(
+        openGlView
+    ) {
+        rtspServer = RtspServer(openGlView.context, connectCheckerRtsp, port)
+    }
 
-  constructor(lightOpenGlView: LightOpenGlView, connectCheckerRtsp: ConnectCheckerRtsp,
-    port: Int) : super(lightOpenGlView) {
-    rtspServer = RtspServer(lightOpenGlView.context, connectCheckerRtsp, port)
-  }
+    constructor(
+        lightOpenGlView: LightOpenGlView, connectCheckerRtsp: ConnectCheckerRtsp,
+        port: Int
+    ) : super(lightOpenGlView) {
+        rtspServer = RtspServer(lightOpenGlView.context, connectCheckerRtsp, port)
+    }
 
-  constructor(context: Context, useOpengl: Boolean, connectCheckerRtsp: ConnectCheckerRtsp,
-    port: Int) : super(context, useOpengl) {
-    rtspServer = RtspServer(context, connectCheckerRtsp, port)
-  }
+    constructor(
+        context: Context, useOpengl: Boolean, connectCheckerRtsp: ConnectCheckerRtsp,
+        port: Int
+    ) : super(context, useOpengl) {
+        rtspServer = RtspServer(context, connectCheckerRtsp, port)
+    }
 
-  fun setVideoCodec(videoCodec: VideoCodec) {
-    videoEncoder.type =
-      if (videoCodec == VideoCodec.H265) CodecUtil.H265_MIME else CodecUtil.H264_MIME
-  }
+    fun setVideoCodec(videoCodec: VideoCodec) {
+        videoEncoder.type =
+            if (videoCodec == VideoCodec.H265) CodecUtil.H265_MIME else CodecUtil.H264_MIME
+    }
 
-  fun getEndPointConnection(): String = "rtsp://${rtspServer.serverIp}:${rtspServer.port}/"
+    fun getEndPointConnection(): String = "rtsp://${rtspServer.serverIp}:${rtspServer.port}/"
 
-  override fun setAuthorization(user: String, password: String) { //not developed
-  }
+    override fun setAuthorization(user: String, password: String) { //not developed
+    }
 
-  fun startStream() {
-    super.startStream("")
-  }
+    fun startStream() {
+        super.startStream("")
+    }
 
-  override fun prepareAudioRtp(isStereo: Boolean, sampleRate: Int) {
-    rtspServer.isStereo = isStereo
-    rtspServer.sampleRate = sampleRate
-  }
+    override fun prepareAudioRtp(isStereo: Boolean, sampleRate: Int) {
+        rtspServer.isStereo = isStereo
+        rtspServer.sampleRate = sampleRate
+    }
 
-  override fun startStreamRtp(url: String) { //unused
-  }
+    override fun startStreamRtp(url: String) { //unused
+    }
 
-  override fun stopStreamRtp() {
-    rtspServer.stopServer()
-  }
+    override fun stopStreamRtp() {
+        rtspServer.stopServer()
+    }
 
-  override fun getAacDataRtp(aacBuffer: ByteBuffer, info: MediaCodec.BufferInfo) {
-    rtspServer.sendAudio(aacBuffer, info)
-  }
+    override fun getAacDataRtp(aacBuffer: ByteBuffer, info: MediaCodec.BufferInfo) {
+        rtspServer.sendAudio(aacBuffer, info)
+    }
 
-  override fun onSpsPpsVpsRtp(sps: ByteBuffer, pps: ByteBuffer, vps: ByteBuffer?) {
-    val newSps = sps.duplicate()
-    val newPps = pps.duplicate()
-    val newVps = vps?.duplicate()
-    rtspServer.setVideoInfo(newSps, newPps, newVps)
-    rtspServer.startServer()
-  }
+    override fun onSpsPpsVpsRtp(sps: ByteBuffer, pps: ByteBuffer, vps: ByteBuffer?) {
+        val newSps = sps.duplicate()
+        val newPps = pps.duplicate()
+        val newVps = vps?.duplicate()
+        rtspServer.setVideoInfo(newSps, newPps, newVps)
+        rtspServer.startServer()
+    }
 
-  override fun getH264DataRtp(h264Buffer: ByteBuffer, info: MediaCodec.BufferInfo) {
-    rtspServer.sendVideo(h264Buffer, info)
-  }
+    override fun getH264DataRtp(h264Buffer: ByteBuffer, info: MediaCodec.BufferInfo) {
+        rtspServer.sendVideo(h264Buffer, info)
+    }
 
-  /**
-   * Unused functions
-   */
-  @Throws(RuntimeException::class)
-  override fun resizeCache(newSize: Int) {
-  }
+    /**
+     * Unused functions
+     */
+    @Throws(RuntimeException::class)
+    override fun resizeCache(newSize: Int) {
+    }
 
-  override fun shouldRetry(reason: String?): Boolean = false
+    override fun shouldRetry(reason: String?): Boolean = false
 
-  override fun reConnect(delay: Long) {
-  }
+    override fun reConnect(delay: Long) {
+    }
 
-  override fun setReTries(reTries: Int) {
-  }
+    override fun setReTries(reTries: Int) {
+    }
 
-  override fun getCacheSize(): Int = 0
+    override fun getCacheSize(): Int = 0
 
-  override fun getSentAudioFrames(): Long = 0
+    override fun getSentAudioFrames(): Long = 0
 
-  override fun getSentVideoFrames(): Long = 0
+    override fun getSentVideoFrames(): Long = 0
 
-  override fun getDroppedAudioFrames(): Long = 0
+    override fun getDroppedAudioFrames(): Long = 0
 
-  override fun getDroppedVideoFrames(): Long = 0
+    override fun getDroppedVideoFrames(): Long = 0
 
-  override fun resetSentAudioFrames() {
-  }
+    override fun resetSentAudioFrames() {
+    }
 
-  override fun resetSentVideoFrames() {
-  }
+    override fun resetSentVideoFrames() {
+    }
 
-  override fun resetDroppedAudioFrames() {
-  }
+    override fun resetDroppedAudioFrames() {
+    }
 
-  override fun resetDroppedVideoFrames() {
-  }
+    override fun resetDroppedVideoFrames() {
+    }
 }
