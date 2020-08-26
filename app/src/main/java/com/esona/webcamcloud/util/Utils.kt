4cf857f57e8c3cb8a6611f303a6046ff70924866
vladimir <gaidukov76@gmail.com>
2020-08-26 17:53:01 +0500

    debug and update

--- app/src/main/java/com/esona/webcamcloud/util/Utils.kt
+++ app/src/main/java/com/esona/webcamcloud/util/Utils.kt
@@ -19,7 +19,6 @@ object Utils {
         res.port= prefs.getInt("port", 1935)
         res.rate= prefs.getInt("rate", 15)
         res.camera= prefs.getInt("camera", 0)
-        res.h264= prefs.getBoolean("h264", false)
         return res
     }
 
@@ -32,7 +31,6 @@ object Utils {
         prefs.putInt("lang", data.lang)
         prefs.putInt("port", data.port)
         prefs.putInt("camera", data.camera)
-        prefs.putBoolean("h264", data.h264)
         prefs.commit()
     }
 
